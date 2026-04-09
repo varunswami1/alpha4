08f7cfa847437815cddf7f6bdc7bbd7224d127ed
LALIT <LALIT.23FE10CDS00461@muj.manipal.edu>
2026-04-09 13:30:28 +0530

    Remove DiseaseAlerts from Dashboard component
    Removed DiseaseAlerts component from the Dashboard.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,6 @@ import TodoCalendar from "@/components/dashboard/TodoCalendar";
 import TodaysTasks from "@/components/dashboard/TodaysTasks";
 import PlantInformation from "@/components/dashboard/PlantInformation";
 import WeatherInformation from "@/components/dashboard/WeatherInformation";
-import DiseaseAlerts from "@/components/dashboard/DiseaseAlerts";
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
 
 const Dashboard = () => {
@@ -45,14 +44,6 @@ const Dashboard = () => {
           <WeatherInformation />
         </motion.div>
         
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-          className="col-span-1 md:col-span-2"
-        >
-          <DiseaseAlerts />
-        </motion.div>
       </div>
     </DashboardLayout>
   );
